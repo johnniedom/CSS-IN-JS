075e4f619547ec290bf7c26901e8b7058fcecf5d
JohnnieDom <johnniedom1@gmail.com>
2023-03-29 18:43:44 +0100

    📦 chore(package.json): add styled-components dependency 🚀 feat(App.jsx): add FQA and LoginForm components, add GlobalStyles ✨ feat(FQA.jsx): create FQA component with styled components
    ✨ feat(login.jsx): add LoginForm component with styled-components
    
    🚀 feat(test.jsx): add range and sum functions to calculate range and sum of numbers in an array
    
    🎉 feat(vite.config.js): add vite configuration file with react plugin

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
+import FQA from "./Components/FQA/FQA";
+import LoginForm from "./Components/Login/login";
+import GlobalStyles from "./GlobalStyles/GlobalStyles";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-   <div>
-    
-   </div>
-  )
+    <>
+      {" "}
+      <div>
+        <FQA
+          question="What does “CSS” stand for?"
+          answer="Cool Styling StrategyI've built so many little FAQ components over the years.
+           Let's convert this one! The “CSS” tab should be empty, by the time you're finished.
+          Note: You don't need to import styled, it's already in scope!"
+        />
+        <LoginForm/>
+
+      </div>
+         <section>
+         
+         </section>
+
+      <GlobalStyles />
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/Components/Login/login.jsx
@@ -0,0 +1,100 @@
+import styled from "styled-components";
+
+function LoginForm({ handleSubmit }) {
+  return (
+    <Form id="login-form" onSubmit={handleSubmit}>
+      <Labels>
+        Email:
+        <Data type="email" placeholder="me@you.com" />
+      </Labels>
+      <Labels>
+        Password:
+        <Data type="password" />
+      </Labels>
+      <Button type="submit">Log In</Button>
+    </Form>
+  );
+}
+
+const Form = styled.form`
+  width: max-content;
+  margin: 16px auto;
+  border: 1px dotted silver;
+  padding: 26px 32px 32px;
+  border-radius: 2px;
+`;
+
+const Labels = styled.label`
+  display: block;
+  margin-bottom: 24px;
+`;
+
+const Data = styled.input`
+  display: block;
+  width: 175px;
+  border: 1px solid black;
+  border-bottom-width: 0.5px;
+  padding: 6px 8px;
+  margin-top: 4px;
+  border-radius: 2px 2px 3px 3px;
+
+  &:focus {
+    outline: 1px inset slateRed;
+    outline-offset: 1px;
+    border-color: transparent;
+  }
+`;
+
+//Props Is the short form of Properties
+
+const Button = styled.button.attrs((props) => ({ type: "submit" }))`
+  /* your styles here */
+  display: block;
+  margin-top: 40px;
+  width: 100%;
+  background: black;
+  color: white;
+  padding: 8px;
+  border: none;
+  font-size: 1rem;
+  font-weight: 600;
+  text-transform: uppercase;
+  border-radius: 3px;
+
+  &:hover {
+    outline: none;
+    background: red;
+  }
+
+  &:focus {
+    outline: 3px auto blue;
+    background: blue;
+    outline-offset: 2px;
+  }
+`;
+
+/**
+
+
+#login-form button[type=submit] {
+  display: block;
+  margin-top: 40px;
+  width: 100%;
+  background: black;
+  color: white;
+  padding: 8px;
+  border: none;
+  font-size: 1rem;
+  font-weight: 600;
+  text-transform: uppercase;
+  border-radius: 3px;
+}
+
+#login-form button[type=submit]:focus {
+  outline: 3px auto blue;
+  background: blue;
+  outline-offset: 2px;
+}
+ */
+
+export default LoginForm;
